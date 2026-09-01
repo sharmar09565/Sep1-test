91174c354576faf8b2cdfaf845d933b6c2ef03a6
sharmar09565 <sharmar09565@gmail.com>
2026-09-01 16:02:49 +0530

    sfdgfgn

--- Frontend/src/Features/Home/Home.jsx
+++ Frontend/src/Features/Home/Home.jsx
@@ -2,36 +2,74 @@ import axios from "axios"
 import { useEffect } from "react"
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import Fuse from 'fuse.js'
 
 
 const Home = () => {
 
-    const [events,setEvents] = useState([])
+    const [events, setEvents] = useState([])
+    const [search, setSearch] = useState('')
+    const [debounce, setDebounce] = useState('')
 
-    const fetchEvents = async() =>{
-        try{
+
+    const fetchEvents = async () => {
+        try {
             const response = await axios.get('http://localhost:5000/events')
 
             setEvents(response.data)
-        } catch(error){
+        } catch (error) {
             console.error(error)
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         fetchEvents()
-    },[])
+    }, [])
+
+    useEffect(() => {
+
+        const timer = setTimeout(() => {
+            setDebounce(search);
+        }, 500);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    }, [search]);
+
+    useEffect(() => {
+
+        const fuse = new Fuse(events, {
+            keys: ["name"],
+            threshold: 0.4
+        });
+
+        if (debounce === "") {
+            setEvents(events);
+        } 
+        else {
+            const searchEvents = fuse.search(debounce);
+
+            setEvents(
+                searchEvents.map(ev => ev.item)
+            );
+        }
+
+    }, [debounce]);
+
 
     return (
         <div>
+            <input type="text" onChange={(e) => setSearch(e.target.value)} />
             {
-                events.map(ev=>(
+                events.map(ev => (
                     <Link to={`/event/:${ev.id}`} key={ev.id}>
-                    <h1>{ev.EventName}</h1>
-                    <p> <b>Category:</b> {ev.Category}</p>
-                    <p> <b>Location:</b> {ev.Location}</p>
-                    <p> <b>Date:</b> {ev.Date}</p>
-                    <p> <b>Description:</b> {ev.Description}</p>
+                        <h1>{ev.EventName}</h1>
+                        <p> <b>Category:</b> {ev.Category}</p>
+                        <p> <b>Location:</b> {ev.Location}</p>
+                        <p> <b>Date:</b> {ev.Date}</p>
+                        <p> <b>Description:</b> {ev.Description}</p>
                     </Link>
                 ))
             }
