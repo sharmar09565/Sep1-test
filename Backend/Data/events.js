53078805a1ce027c53bfb0f1d325c5ff1b0a1704
sharmar09565 <sharmar09565@gmail.com>
2026-09-01 14:59:53 +0530

    sadf

---
+++ Backend/Data/events.js
@@ -0,0 +1,42 @@
+const events = [
+    {
+        id: 1,
+        EventName:'SIH',
+        Category:'Ideathon',
+        Location:'SVIET',
+        Date:'12 sep 2026',
+        Description: 'ghfgdffbg  ghgndgdfhgb ghgrhfgdhfnfdghgf'
+    },
+    {
+        id: 2,
+        EventName:'AlgolYmpia',
+        Category:'Hackathon',
+        Location:'CU',
+        Date:'2 sep 2026',
+        Description: 'ghfgdffbg  ghgndgdfhgb ghgrhfgdhfnfdghgf'
+    },
+    {
+        id: 3,
+        EventName:'Sportsmania',
+        Category:'Sports',
+        Location:'SVIET',
+        Date:'12 march 2026',
+        Description: 'ghfgdffbg  ghgndgdfhgb ghgrhfgdhfnfdghgf'
+    },
+    {
+        id: 4,
+        EventName:'Ideaspark',
+        Category:'Ideathon',
+        Location:'SVIET',
+        Date:'12 sep 2026',
+        Description: 'ghfgdffbg  ghgndgdfhgb ghgrhfgdhfnfdghgf'
+    },
+    {
+        id: 5,
+        EventName:'Comicstan',
+        Category:'Ideathon',
+        Location:'Thaapar',
+        Date:'12 jan 2026',
+        Description: 'ghfgdffbg  ghgndgdfhgb ghgrhfgdhfnfdghgf'
+    }
+]
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import users from './Data/users'
+import events from './Data/events'
 
 const app = express()
 
@@ -29,8 +30,32 @@ app.post('/',ValidateUser,(req,res)=>{
     res.status(201).json(newUser)
 })
 
+app.get('/events',(req,res)=>{
+    res.status(200).json(events)
+})
 
+app.get('/events/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    const event = events.find((event)=>{
+        event.id===id
+    })
+    res.status(200).json(event)
+})
 
+app.put('/events/:id',(req,res)=>{
+    const id = Number(req.params.id)
+    let event = events.find((event)=>{
+        event.id===id
+    })
+    if(!event){
+        return res.status(404).json({message:"User not found"})
+    }
+    event.EventName = req.body.EventName,
+    event.Category = req.body.Category,
+    event.Location = req.body.Location,
+    event.Date = req.body.Date,
+    event.Description = req.body.Description
+})
 
 app.listen(5000,()=>{
     console.log('Server is running on 5000')
